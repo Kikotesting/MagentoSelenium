65798111c73ec5b3e0371d49e0349c0ded96e9d5
klimenth <klimenth@bitbucket.securegroup.zone>
2023-02-03 15:55:53 +0200

    2 more elements

--- src/main/java/utils/CartContainer.java
+++ src/main/java/utils/CartContainer.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.support.FindBy;
 
 public abstract class CartContainer {
     @FindBy(className = "showcart") WebElement cartContainerButton;
+    @FindBy(xpath = "//*[@id=\"top-cart-btn-checkout\"]") WebElement cartContainerCheckoutButton;
+    @FindBy(xpath = "//*[@id=\"minicart-content-wrapper\"]/div[2]/div[2]/div/span/span") WebElement cartSubtotalText;
+    @FindBy(xpath = "//*[@id=\"minicart-content-wrapper\"]/div[2]/div[5]/div/a") WebElement viewAndEditTextButton;
     @FindBy(xpath = "//div[4]/aside[2]/div[2]/footer/button[1]") WebElement modalCancel;
     @FindBy(xpath = "//div[4]/aside[2]/div[2]/footer/button[2]") WebElement modalConfirm;
     @FindBy(xpath = "//*[@id=\"minicart-content-wrapper\"]/div[2]/strong") WebElement emptyCartMessage;
     @FindBy(xpath = "//*[@id=\"btn-minicart-close\"]") WebElement closeButton;
-
 }
